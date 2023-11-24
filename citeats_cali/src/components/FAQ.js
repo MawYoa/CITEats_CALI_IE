4a03141b105d2caff7aa8f5d5d15fc9461a3a2ca
EarlClaro <earlclaro@gmail.com>
2023-11-24 14:46:27 +0800

    update

---
+++ citeats_cali/src/components/FAQ.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+const FAQ = () => {
+    return (
+<>
+<div style={{ textAlign: 'right',padding: '0 280px'  }}>
+<h2 style={{ textAlign: 'left' }}>Frequently Asked Questions</h2>
+<br></br>
+<p style={{ textAlign: 'left' ,fontWeight:'bold'}}>
+  Can I review the food I ate from the restaurant shown on the site?
+</p>
+<p style={{ textAlign: 'left' }}>
+  Yes, you can review the food you ate, but you must stay honest.
+</p>
+
+<p style={{ textAlign: 'left' ,fontWeight:'bold'}}>How to find the location of the Restaurant:</p>
+<ul style={{ textAlign: 'left' }}>
+  <li>Browse for Restaurants</li>
+  <li>Click desired cuisine type</li>
+  <li>Click your favored restaurant</li>
+  <li>Click the restaurant details</li>
+  <li>Click the location of the Restaurant</li>
+  <li>Locate the restaurant's location on Google Maps</li>
+</ul>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+</div>
+
+
+
+
+</>
+    );
+  };
+  
+
+export default FAQ;
--- citeats_cali/src/pages/Home.js
+++ citeats_cali/src/pages/Home.js
@@ -2,11 +2,9 @@
 
 import React from 'react';
 import styled from 'styled-components';
-//import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-
+import FAQ from '../components/FAQ';
 
 import './Home.css';
 import { Link } from 'react-router-dom';
@@ -240,64 +238,42 @@ cursor: pointer;
         <h2>Popular near you</h2>
         <PopularNearYou />
         <br></br>
-        <div style={{ alignItems: 'left', marginTop: 0, marginBottom: 0 }}>
-          <h3> Latest Reviews</h3>
-          <h4 style={{color:'gold'}}> 4.4 overall rating</h4>
-        </div>
-      
-      <SeeAllReviewsButton><StyledLink to="/Reviews">See All Reviews</StyledLink></SeeAllReviewsButton>
-      <br></br>
-        <br></br>
- 
-      <ReviewCard>
-        <p style={{color:'maroon',fontWeight:'bold'}}>YEZETH</p>
-        <p style={{color:'gold'}}>5.0</p>
-        <p>1 minute ago, reviewed on LockDown BU ...</p>
-        <p>Great food! Great food!</p>
-      </ReviewCard>
-      <ReviewCard>
-        <p style={{color:'maroon',fontWeight:'bold'}}>Kristhyn</p>
-        <p style={{color:'gold'}}>5.0</p>
-        <p>2 minutes ago, reviewed on Mcdonalds So-...</p>
-        <p>Awesome as always!</p>
-      </ReviewCard>
-      <ReviewCard>
-        <p style={{color:'maroon',fontWeight:'bold'}}>Nikka</p>
-        <p style={{color:'gold'}}>5.0</p>
-        <p>3 minutes ago, reviewed on Elsa Silogan-...</p>
-        <p>Gamay ra kaayo ang creamy scallops</p>
-      </ReviewCard>
-      <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
+      <div>
+            <div style={{ alignItems: 'left', marginTop: 0, marginBottom: 0 }}>
+              <h3> Latest Reviews</h3>
+              <h4 style={{color:'gold'}}> 4.4 overall rating</h4>
+            </div>
+          
+          <SeeAllReviewsButton><StyledLink to="/Reviews">See All Reviews</StyledLink></SeeAllReviewsButton>
+          <br></br>
+            <br></br>
+    
+          <ReviewCard>
+            <p style={{color:'maroon',fontWeight:'bold'}}>YEZETH</p>
+            <p style={{color:'gold'}}>5.0</p>
+            <p>1 minute ago, reviewed on LockDown BU ...</p>
+            <p>Great food! Great food!</p>
+          </ReviewCard>
+          <ReviewCard>
+            <p style={{color:'maroon',fontWeight:'bold'}}>Kristhyn</p>
+            <p style={{color:'gold'}}>5.0</p>
+            <p>2 minutes ago, reviewed on Mcdonalds So-...</p>
+            <p>Awesome as always!</p>
+          </ReviewCard>
+          <ReviewCard>
+            <p style={{color:'maroon',fontWeight:'bold'}}>Nikka</p>
+            <p style={{color:'gold'}}>5.0</p>
+            <p>3 minutes ago, reviewed on Elsa Silogan-...</p>
+            <p>Gamay ra kaayo ang creamy scallops</p>
+          </ReviewCard>
+          <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+      </div>
 
       </HomeContainer>
-      <div style={{ textAlign: 'right',padding: '0 280px'  }}>
-          <h2 style={{ textAlign: 'left' }}>Frequently Asked Questions</h2>
-          <br></br>
-          <p style={{ textAlign: 'left' ,fontWeight:'bold'}}>
-            Can I review the food I ate from the restaurant shown on the site?
-          </p>
-          <p style={{ textAlign: 'left' }}>
-            Yes, you can review the food you ate, but you must stay honest.
-          </p>
-
-          <p style={{ textAlign: 'left' ,fontWeight:'bold'}}>How to find the location of the Restaurant:</p>
-          <ul style={{ textAlign: 'left' }}>
-            <li>Browse for Restaurants</li>
-            <li>Click desired cuisine type</li>
-            <li>Click your favored restaurant</li>
-            <li>Click the restaurant details</li>
-            <li>Click the location of the Restaurant</li>
-            <li>Locate the restaurant's location on Google Maps</li>
-          </ul>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-        </div>
+        <FAQ />
         <Footer/>
     </div>
   );
